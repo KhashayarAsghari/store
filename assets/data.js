e97b263563e95e28020c9c0e51b426722783ab94
XERXES <eng.asghari75@gmail.com>
2023-09-19 14:19:40 -0700

    cart counter and list

--- assets/data.js
+++ assets/data.js
@@ -1,4 +1,7 @@
 let root = document.getElementById("root");
+let cartCounter = document.getElementById("cart-counter");
+let cartListRoot = document.getElementById("cart-list-container");
+let cartBtn = document.getElementById("cart-button")
 let PRODUCTS = [];
 let CART = [];
 
@@ -8,11 +11,18 @@ fetch('https://fakestoreapi.com/products')
     .then(()=> {
         renderProducts(PRODUCTS)
     })
+    .then(
+        fetch('https://fakestoreapi.com/carts')
+        .then(res=>res.json())
+        .then(json=>json[0].products.map(item => {
+            return PRODUCTS.find(pItem => pItem.id == item.productId)
+        }))
+        .then(list => {
+            cartCounter.textContent = list.length;
+            renderCartList(list);
+
+    }))
     .catch(console.log("products fetching failed"));
 
-fetch('https://fakestoreapi.com/carts')
-    .then(res=>res.json())
-    .then(json=>CART = json)
-    .then(() => {
-        
-    })
+
+
--- assets/main.js
+++ assets/main.js
@@ -12,5 +12,23 @@ function renderProducts(list) {
     }).join("")
 
     root.innerHTML = template
-    window.pushState('all products', "?allProducts")
 }
+
+
+function renderCartList(list) {
+    let template = list.map(item => {
+        return `
+        <div class="cart__item">
+            <img src="${item.image}" alt="" class="cart__item__image">
+            <div class="cart__item__desc">
+                <h2 class="cart__item__desc__title">${item.title}</h2>
+                <span class="cart__item__desc__price">${item.price} $</span>
+            </div>
+        </div>
+        `
+    }).join("")
+
+    cartListRoot.innerHTML = template
+}
+
+cartBtn.addEventListener("click", ()=> cartListRoot.classList.toggle("hidden"))
